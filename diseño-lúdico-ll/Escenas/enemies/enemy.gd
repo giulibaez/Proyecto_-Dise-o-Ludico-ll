bd44fc150f0b4d05c93ddaf97f0225d1506eb0b7
giulibaez <giulibaez56@gmail.com>
2025-06-03 13:13:10 -0300

    creacion de un enemigo base

--- diseño-lúdico-ll/Escenas/enemies/enemy.gd
+++ diseño-lúdico-ll/Escenas/enemies/enemy.gd
@@ -1,6 +1,23 @@
 extends CharacterBody2D
 
 var health = 2
+var speed = 50
+var player_chase= false
+var player = null
+
+
+func _physics_process(delta: float) -> void:
+	if player_chase:
+		position += (player.position - position)/speed 
+		
+		$AnimatedSprite2D.play("walk")
+		if (player.position.x - position.x) <0:
+			$AnimatedSprite2D.flip_h = false
+		else:
+			$AnimatedSprite2D.flip_h = true
+	else:
+		$AnimatedSprite2D.play("idle")
+
 
 func take_damage(amount: int):
 	health -= amount
@@ -10,3 +27,13 @@ func take_damage(amount: int):
 func die():
 	# agregar soltar item, efectos, animaciones, etc.
 	queue_free()
+
+
+
+func _on_detection_area_body_entered(body: Node2D) -> void:
+	player = body
+	player_chase = true
+
+func _on_detection_area_body_exited(body: Node2D) -> void:
+	player = null
+	player_chase = false 
