9bded787bba79bcc09d9f6e05a05fa9afe1853c8
giulibaez <giulibaez56@gmail.com>
2025-06-16 13:29:54 -0300

    enemigo recibe daño

--- diseño-lúdico-ll/Escenas/enemies/enemy.gd
+++ diseño-lúdico-ll/Escenas/enemies/enemy.gd
@@ -1,9 +1,12 @@
 extends CharacterBody2D
 
-var health = 2
+var health = 3
 var speed = 65
 var player_chase= false
 var player = null
+var player_inattack_zone= false
+var can_take_damage = true
+var can_attack = true
 
 @onready var anim_enemy = $AnimatedSprite2D
 
@@ -11,8 +14,11 @@ var player = null
 
 func _physics_process(delta: float) -> void:
 	move()
+	deal_whit_damage()
 
 func move():
+	if anim_enemy.is_playing() and anim_enemy.animation in ["hurt", "death","attack"]:
+		return
 	if player_chase:
 		position += (player.position - position)/speed 
 		anim_enemy.play("walk")
@@ -26,12 +32,22 @@ func move():
 
 func take_damage(amount: int):
 	health -= amount
+	print("daño")
+	$TakeDamageTimer.start()
+	can_take_damage= false
+	can_attack = false
 	anim_enemy.play("hurt")
-	if health <= 0:
+	if player != null:
+		var knockback_dir = (position - player.position).normalized()
+		position += knockback_dir * 40
+		print("aplicando knocknaback")
+	if health == 0:
 		die()
 
 func die():
 	anim_enemy.play("death")
+	print("animacion de muerte")
+	await anim_enemy.animation_finished
 	queue_free()
 
 func enemy():
@@ -40,13 +56,33 @@ func enemy():
 func _on_detection_area_body_entered(body: Node2D) -> void:
 	player = body
 	player_chase = true
-	attack_player()
 
 
 func _on_detection_area_body_exited(body: Node2D) -> void:
 	player = null
 	player_chase = false 
 
-func attack_player():
-	if player_chase:
-		anim_enemy.play("attack")
+
+func _on_hit_box_area_body_entered(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_inattack_zone = true
+		if can_attack:
+			anim_enemy.play("attack")
+
+
+func _on_hit_box_area_body_exited(body: Node2D) -> void:
+	if body.has_method("player"):
+		player_inattack_zone = false
+		
+func deal_whit_damage():
+	if player_inattack_zone and global.player_current_attack == true:
+		if can_take_damage == true:
+			take_damage(1)
+
+
+func _on_take_damage_timer_timeout() -> void:
+	can_take_damage = true
+	can_attack = true
+
+func attack():
+	anim_enemy.play("attack")
--- diseño-lúdico-ll/Escenas/player/player.gd
+++ diseño-lúdico-ll/Escenas/player/player.gd
@@ -1,161 +1,16 @@
-#extends CharacterBody2D
-#var move_speed = 100
-#var is_left = true
-#@onready var Animated_sprite = $AnimatedSprite2D
-#var current_direction = "none"
-#
-#var max_health = 3
-#var current_health= 3
-#var enemy_inattack_range = false
-#var enemy_attack_cooldown = true
-#var player_alive= true
-#@onready var heart_sound = $HeartSound
-#@onready var heart_display = $Node/Vida
-#@onready var hitbox_area = $HitBoxArea
-#@onready var timer = $AttackCooldown
-#
-#
-#
-#
-#func _ready() -> void:
-	#$AnimatedSprite2D.play("front_idle")
-#
-#
-#@warning_ignore("unused_parameter")
-#func _physics_process(delta: float) -> void:
-	#move()
-	#move_and_slide()
-	#enemy_attack()
-#
-#
-#func move():
-	## Movimiento horizontal
-	#if Input.is_action_pressed("move_right"):
-		#current_direction = "right"
-		#play_animation(1)
-		#velocity.x = move_speed
-		#velocity.y = 0
-	#elif Input.is_action_pressed("move_left"):
-		#current_direction = "left"
-		#play_animation(1)
-		#velocity.x = -move_speed
-		#velocity.y = 0
-	 ## Movimiento vertical
-	#elif Input.is_action_pressed("move_down"):
-		#current_direction = "down"
-		#play_animation(1)
-		#velocity.y = move_speed
-		#velocity.x = 0
-	#elif Input.is_action_pressed("move_up"):
-		#current_direction = "up"
-		#play_animation(1)
-		#velocity.y = -move_speed
-		#velocity.x = 0
-	#else:
-		#play_animation(0)
-		#velocity.x = 0
-		#velocity.y = 0
-#
-#
-#func play_animation(movement):
-	#var direction = current_direction
-	#var animation = $AnimatedSprite2D
-	#
-	#if direction =="right":
-		#animation.flip_h = true
-		#if movement == 1:
-			#animation.play("side_walk")
-		#elif movement == 0:
-			#animation.play("side_idle")
-		#
-	#if direction =="left":
-		#animation.flip_h = false
-		#if movement == 1:
-			#animation.play("side_walk")
-		#elif movement == 0:
-			#animation.play("side_idle")
-		#
-	#if direction =="down":
-		#if movement == 1:
-			#animation.play("front_walk")
-		#elif movement == 0:
-			#animation.play("front_idle")
-		#
-	#if direction =="up":
-		#if movement == 1:
-			#animation.play("back_walk")
-		#elif movement == 0:
-			#animation.play("back_idle")
-#
-#func  take_damage(cant: int):
-	#current_health -= cant
-	#current_health = clamp(current_health, 0 , max_health) #clamp evalua  que la vida actual no sea menor que 0 ni mayor a la vida maxima
-	#
-	#update_hearts_display()
-	#
-	##if current_health == 2:
-		##heart_sound.stream = preload("res://music/sound_heart_player_slow.mp3")
-		##heart_sound.play()
-	##
-	##elif current_health == 1:
-		##heart_sound.stream = preload("res://music/sound_heart_player_fast.mp3")
-		##heart_sound.play()
-	#
-	#if  current_health <= 0:
-		#die()
-#
-#func die():
-	#queue_free() #elimina al jugador de la escena 
-	##mas adelante implementar animacion de muerte y pantalla de game over 
-#
-#func update_hearts_display():
-	#match current_health:
-		#3:
-			#heart_display.frame = 0
-		#2:
-			#heart_display.frame = 1
-		#1:
-			#heart_display.frame = 2
-		#0:
-			#heart_display.frame = 3
-#
-#
-#func player():
-	#pass
-#
-#func _on_hit_box_area_body_entered(body: CharacterBody2D) -> void:
-	#if body.has_method("enemy"):
-		#enemy_inattack_range = true
-		#print("acatoy")
-#
-#
-#func _on_hit_box_area_body_exited(body: CharacterBody2D) -> void:
-	#if body.is_in_group("enemies"):
-		#enemy_inattack_range = false
-#
-#func enemy_attack():
-	#if enemy_inattack_range and enemy_attack_cooldown and player_alive:
-		#take_damage(1)  # Aplica daño
-		#enemy_attack_cooldown = false
-		#timer.start()  # Inicia el temporizador
-		#print("Ataque enemigo! Vida actual: ", current_health)
-#
-#func _on_attack_cooldown_timeout() -> void:
-	#enemy_attack_cooldown = true  # Restablece el cooldown cuando el timer termina
-	#print("Cooldown terminado, enemigo puede atacar de nuevo")
-
 extends CharacterBody2D
 
 var move_speed = 100
 var is_left = true
-@onready var animated_sprite = $AnimatedSprite2D
 var current_direction = "none"
-
 var max_health = 3
 var current_health = 3
 var enemy_inattack_range = false
 var enemy_attack_cooldown = true
 var player_alive = true
+var attack_inprogress = false 
+
+@onready var animated_sprite = $AnimatedSprite2D
 @onready var heart_sound_slow = $Node/HeartSoundSlow
 @onready var heart_sound_fast = $Node/HeartSoundFast
 @onready var heart_display = $Node/Vida
@@ -164,7 +19,6 @@ var player_alive = true
 
 func _ready() -> void:
 	animated_sprite.play("front_idle")
-	timer.wait_time = 1.0  # Tiempo de espera entre ataques (ajusta si necesitas)
 	timer.one_shot = true  # El timer se ejecuta una vez por ataque
 	if not timer.is_connected("timeout", _on_attack_cooldown_timeout):
 		timer.connect("timeout", _on_attack_cooldown_timeout)  # Conecta la señal en código
@@ -174,6 +28,7 @@ func _physics_process(delta: float) -> void:
 		move()
 		move_and_slide()
 		enemy_attack()
+		attack()
 
 func move():
 	if Input.is_action_pressed("move_right"):
@@ -210,23 +65,27 @@ func play_animation(movement):
 		if movement == 1:
 			animation.play("side_walk")
 		else:
-			animation.play("side_idle")
+			if attack_inprogress == false:
+				animation.play("side_idle")
 	if direction == "left":
 		animation.flip_h = false
 		if movement == 1:
 			animation.play("side_walk")
 		else:
-			animation.play("side_idle")
+			if attack_inprogress == false:
+				animation.play("side_idle")
 	if direction == "down":
 		if movement == 1:
 			animation.play("front_walk")
 		else:
-			animation.play("front_idle")
+			if attack_inprogress == false:
+				animation.play("front_idle")
 	if direction == "up":
 		if movement == 1:
 			animation.play("back_walk")
 		else:
-			animation.play("back_idle")
+			if attack_inprogress == false:
+				animation.play("back_idle")
 
 func take_damage(cant: int):
 	if not player_alive:
@@ -255,7 +114,6 @@ func die():
 	if player_alive == false:
 		animated_sprite.play("death")
 		await animated_sprite.animation_finished
-		print("memoori")
 	queue_free()
 
 func update_hearts_display():
@@ -272,18 +130,56 @@ func update_hearts_display():
 func _on_hit_box_area_body_entered(body: Node2D) -> void:
 	if body.has_method("enemy") and not enemy_inattack_range:
 		enemy_inattack_range = true
+		print("enemigo detectado n hitbox")
+		if enemy_attack_cooldown:
+			$EnemyDamageDelayTimer.start()
+			print("inicia delay")
 
 func _on_hit_box_area_body_exited(body: Node2D) -> void:
 	if body.has_method("enemy"):
 		enemy_inattack_range = false
 
 func enemy_attack():
+	pass
+
+func _on_attack_cooldown_timeout() -> void:
+	enemy_attack_cooldown = true
+
+func attack():
+	var dir = current_direction
+	
+	if Input.is_action_just_pressed("attack"):
+		global.player_current_attack = true
+		attack_inprogress = true
+		if dir == "right":
+			animated_sprite.flip_h = true
+			animated_sprite.play("side_attack")
+			$DealAttackTimer.start()
+		if dir == "left":
+			animated_sprite.flip_h = false
+			animated_sprite.play("side_attack")
+			$DealAttackTimer.start()
+		if dir == "down":
+			animated_sprite.play("front_attack")
+			$DealAttackTimer.start()
+		if dir == "up":
+			animated_sprite.play("back_attack")
+			$DealAttackTimer.start()
+
+func _on_deal_attack_timer_timeout() -> void:
+	$DealAttackTimer.stop()
+	global.player_current_attack = false
+	attack_inprogress = false 
+
+func player():
+	pass
+
+
+func _on_enemy_damage_delay_timer_timeout() -> void:
 	if enemy_inattack_range and enemy_attack_cooldown and player_alive:
 		take_damage(1)
 		enemy_attack_cooldown = false
 		timer.start()
-		print(" Vida actual: ", current_health)
-
-func _on_attack_cooldown_timeout() -> void:
-	enemy_attack_cooldown = true
-	print("Cooldown terminado, enemigo puede atacar de nuevo")
+		print("daño aplicado al jugador")
+	else:
+		print("no se aplica daño al jugador")
